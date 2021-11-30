26fab009e2ee8e0e5f05e389d82ef5ac868851ed
Shahzad Mughal <smughal55@users.noreply.github.com>
2021-12-01 00:30:45 +0400

    first commit

---
+++ scripts/deploy_and_upgrade.py
@@ -0,0 +1,46 @@
+from brownie.network.contract import Contract
+from scripts.helpful_scripts import get_account, encode_function_data, upgrade
+from brownie import (
+    network,
+    Box,
+    ProxyAdmin,
+    TransparentUpgradeableProxy,
+    Contract,
+    BoxV2,
+)
+
+
+def main():
+    account = get_account()
+    print(f"Deploying to {network.show_active()}")
+    box = Box.deploy({"from": account}, publish_source=True)
+    # print(box.retrieve())
+
+    #  if you do have a proxy admin and use some type of defi protocol, proxy admin shoud be a type of multisig e.g. Gnosis-safe, (not done here)
+
+    proxy_admin = ProxyAdmin.deploy({"from": account}, publish_source=True)
+
+    # initializer = box.store, 1
+    box_encoded_initializer_function = encode_function_data()
+
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address,
+        proxy_admin.address,
+        box_encoded_initializer_function,
+        {"from": account, "gas_limit": 1000000},
+        publish_source=True,
+    )
+    print(f"Proxy deployed to {proxy}, you can now upgrade to v2!")
+    proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
+    proxy_box.store(1, {"from": account})
+    # print(proxy_box.retrieve())
+    # upgrade
+    box_v2 = BoxV2.deploy({"from": account}, publish_source=True)
+    upgrade_transaction = upgrade(
+        account, proxy, box_v2.address, proxy_admin_contract=proxy_admin
+    )
+    upgrade_transaction.wait(1)
+    print("Proxy has been upgraded!")
+    proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
+    proxy_box.increment({"from": account})
+    print(proxy_box.retrieve())
---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,76 @@
+from brownie import (
+    accounts,
+    network,
+    config,
+)
+from brownie.project.main import new
+import eth_utils
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev", "mainnet-fork-aave"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+
+def get_account(index=None, id=None):
+    # accounts[0]
+    # accounts.add("env")
+    # accounts.load("id")
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    return accounts.add(config["wallets"]["from_key"])
+
+
+# initializer=box.store, 1
+def encode_function_data(initializer=None, *args):
+    """Encodes the function call so we can work with an initializer.
+    Args:
+        initializer ([brownie.network.contract.ContractTx], optional):
+        The initializer function we want to call. Example: `box.store`.
+        Defaults to None.
+        args (Any, optional):
+        The arguments to pass to the initializer function
+    Returns:
+        [bytes]: Return the encoded bytes.
+    """
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
+
+
+def upgrade(
+    account,
+    proxy,
+    new_implementation_address,
+    proxy_admin_contract=None,
+    initializer=None,
+    *args
+):
+    transaction = None
+    if proxy_admin_contract:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy_admin_contract.upgradeAndCall(
+                proxy.address,
+                new_implementation_address,
+                encoded_function_call,
+                {"from": account},
+            )
+        else:
+            transaction = proxy_admin_contract.upgrade(
+                proxy.address, new_implementation_address, {"from": account}
+            )
+    else:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy.upgradeToAndCall(
+                new_implementation_address, encoded_function_call, {"from": account}
+            )
+        else:
+            transaction = proxy.upgradeTo(new_implementation_address, {"from": account})
+    return transaction
